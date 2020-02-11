91ef5050a58337c29a86545aaf7d79f3a5103ad4
Qinyuan <qinyuan.zhang@ericsson.com>
2020-02-11 10:53:42 +0000

    Initial commit

---
+++ src/main/java/com/tonyzhang/touchfish/TouchFishApplication.java
@@ -0,0 +1,13 @@
+package com.tonyzhang.touchfish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouchFishApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TouchFishApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tonyzhang/touchfish/TouchFishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tonyzhang.touchfish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouchFishApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
